dee456336d801cb4c48a38f49a079cb8404c5682
damdadidam <75475421+damdadidam@users.noreply.github.com>
2021-02-16 15:37:27 +0900

    create model package, create class User,RoleType,Board,Reply

---
+++ src/main/java/com/m/salonM/model/Board.java
@@ -0,0 +1,56 @@
+package com.m.salonM.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length=100)
+	private String title;
+	
+	@Lob
+	private String content;
+	
+	private int count;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"board"})
+	private List<Reply> replies;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
---
+++ src/main/java/com/m/salonM/model/RoleType.java
@@ -0,0 +1,5 @@
+package com.m.salonM.model;
+
+public enum RoleType {
+	USER,ADMIN
+}
