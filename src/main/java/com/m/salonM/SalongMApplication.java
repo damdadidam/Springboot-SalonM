aa87cf77f849614f785ad035290f7287da08f720
damdadidam <75475421+damdadidam@users.noreply.github.com>
2021-02-16 12:08:53 +0900

    initial commit

---
+++ src/main/java/com/m/salonM/SalongMApplication.java
@@ -0,0 +1,13 @@
+package com.m.salonM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalongMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalongMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m/salonM/SalongMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.salonM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalongMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
